2931fe33220497f68b2403782a6a80d13f3b5be9
Menik Tennakoon <menik.tennakoon@dialog.lk>
2023-10-09 12:11:09 +0300

    Configure the seciruty

--- src/main/java/com/testing/geologicalsectionsapi/configurations/SecurityConfig.java
+++ src/main/java/com/testing/geologicalsectionsapi/configurations/SecurityConfig.java
@@ -1,41 +1,25 @@
 package com.testing.geologicalsectionsapi.configurations;
 
-//import com.testing.geologicalsectionsapi.util.JwtAuthenticationFilter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //@Configuration
 //@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter
-//{
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception
-//    {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests().anyRequest().authenticated()
-//                .and()
-//                .httpBasic();
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth)
-//            throws Exception
-//    {
-//        auth.inMemoryAuthentication()
-//                .withUser("admin")
-//                .password("password")
-//                .roles("ROLE_USER");
-//    }
-//}
+//@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+                http
+                        .antMatcher("/api/**") // Secure API paths
+                        .authorizeRequests()
+                        .antMatchers(HttpMethod.OPTIONS).permitAll() // Allow pre-flight requests
+                        .anyRequest().authenticated()
+                        .and()
+                        .httpBasic(); // Use Basic Authentication
+        }
+}
